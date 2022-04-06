6384fb3cba8588a736a6359f20704363e22e54a6
Busra Boyali <bsrbyl.bus@gmail.com>
2022-04-06 17:09:57 +0300

    06.04.22

---
+++ src/replit/Q14.java
@@ -0,0 +1,54 @@
+package replit;
+
+import java.util.Scanner;
+
+public class Q14 {
+    public static void main(String[] args) {
+        //Kullanıcıya kilosunu ve boyunu sorun ve kitle indeksini hesaplayan bir program yazın (BMI)
+        //
+        //IPUCU : BMI = Agirlik(kg) / Boy*Boy (m)
+        //
+        //BMI 18,5'in altındaysa zayıfsınız
+        //
+        //BMI 18,5 ile 25 arasında ise kilonuz idealdir
+        //
+        //BMI 25-30 arasındaysa şişmansınız
+        //
+        //BMI 30'dan büyük veya eşitse, obez
+        //
+        //Input:
+        //
+        //Output:
+        //
+        //Agirlik : 71
+        //
+        //Boy : 1,72
+        //
+        //BMI : 23.99945916711736
+        //
+        //Zayifsiniz.
+
+        Scanner scan= new Scanner(System.in);
+        System.out.println("lutfen boyunuzu girin : ");
+        double boy=scan.nextDouble();
+        System.out.println("lutfen kilonuzu girin : ");
+        double kilo=scan.nextDouble();
+        double bmi=kilo/boy*boy;
+
+        if (bmi<18.5){
+            System.out.println(" zayıfsınız");
+
+        }else if (bmi<25){
+            System.out.println("kilonuz ideal ");
+        }else if (bmi<30){
+            System.out.println(" şişmansınız ");
+        }else  if (bmi>=30){
+            System.out.println(" obez ");
+        }else{
+            System.out.println("gecerli bir bilgi giriniz");
+        }
+
+
+    }
+
+}
