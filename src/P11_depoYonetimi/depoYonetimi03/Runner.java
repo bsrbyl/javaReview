c23d01adfb96ec6668292e3c39d2c7e1e6c2f1da
Busra Boyali <bsrbyl.bus@gmail.com>
2022-04-24 01:14:14 +0300

    24.04.22

---
+++ src/P11_depoYonetimi/depoYonetimi03/Runner.java
@@ -0,0 +1,10 @@
+package P11_depoYonetimi.depoYonetimi03;
+
+import static JavaProjects.P11_depoYonetimi.depoYonetimi03.Islemler.menu;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        menu();
+    }
+}
---
+++ src/P11_depoYonetimi05/Runner.java
@@ -0,0 +1,10 @@
+package P11_depoYonetimi05;
+
+public class Runner {
+    public static void main(String[] args) {
+        Islemler.sahteUrunGiris();
+        Islemler.islemlerAnaMenu();
+
+
+    }
+}
